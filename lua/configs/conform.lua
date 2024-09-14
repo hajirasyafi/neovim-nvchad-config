14d8ca1ab40dbe4b8dc83f8622e50631a6829782
Hajir Asyafi <hajirasyafi@Hajirs-MacBook-Pro.local>
2024-09-14 12:47:43 +0700

    update

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -2,67 +2,67 @@
 ---@param ... string
 ---@return string
 local function first(bufnr, ...)
-  local conform = require "conform"
-  for i = 1, select("#", ...) do
-    local formatter = select(i, ...)
-    if conform.get_formatter_info(formatter, bufnr).available then
-      return formatter
-    end
-  end
-  return select(1, ...)
+	local conform = require("conform")
+	for i = 1, select("#", ...) do
+		local formatter = select(i, ...)
+		if conform.get_formatter_info(formatter, bufnr).available then
+			return formatter
+		end
+	end
+	return select(1, ...)
 end
 
 local slow_format_filetypes = {}
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    css = { "prettier" },
-    html = { "prettier" },
-    php = {},
-    javascript = function(bufnr)
-      return { first(bufnr, "eslint", "prettier") }
-    end,
-    typescript = function(bufnr)
-      return { first(bufnr, "eslint", "prettier") }
-    end,
-    javascriptreact = function(bufnr)
-      return { first(bufnr, "eslint", "prettier") }
-    end,
-    typescriptreact = function(bufnr)
-      return { first(bufnr, "eslint", "prettier") }
-    end,
-    json = { "prettier" },
-    go = function(bufnr)
-      return { first(bufnr, "goimports", "gofumpt") }
-    end,
-    templ = { "prettier" },
-    prisma = {},
-    xml = {}
-  },
-  format_on_save = function(bufnr)
-    if slow_format_filetypes[vim.bo[bufnr].filetype] then
-      return
-    end
-    local function on_format(err)
-      if err and err:match "timeout$" then
-        slow_format_filetypes[vim.bo[bufnr].filetype] = true
-      end
-    end
+	formatters_by_ft = {
+		lua = { "stylua" },
+		css = { "prettier" },
+		html = { "prettier" },
+		php = {},
+		javascript = function(bufnr)
+			return { first(bufnr, "eslint", "prettier") }
+		end,
+		typescript = function(bufnr)
+			return { first(bufnr, "eslint", "prettier") }
+		end,
+		javascriptreact = function(bufnr)
+			return { first(bufnr, "eslint", "prettier") }
+		end,
+		typescriptreact = function(bufnr)
+			return { first(bufnr, "eslint", "prettier") }
+		end,
+		json = { "prettier" },
+		go = function(bufnr)
+			return { first(bufnr, "goimports", "gofumpt") }
+		end,
+		templ = { "prettier" },
+		prisma = {},
+		xml = {},
+	},
+	format_on_save = function(bufnr)
+		if slow_format_filetypes[vim.bo[bufnr].filetype] then
+			return
+		end
+		local function on_format(err)
+			if err and err:match("timeout$") then
+				slow_format_filetypes[vim.bo[bufnr].filetype] = true
+			end
+		end
 
-    return { timeout_ms = 1000, lsp_format = "fallback" }, on_format
-  end,
+		return { timeout_ms = 1000, lsp_format = "fallback" }, on_format
+	end,
 
-  format_after_save = function(bufnr)
-    if not slow_format_filetypes[vim.bo[bufnr].filetype] then
-      return
-    end
-    return { timeout_ms = 1000, lsp_format = "fallback" }
-  end,
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+	format_after_save = function(bufnr)
+		if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+			return
+		end
+		return { timeout_ms = 1000, lsp_format = "fallback" }
+	end,
+	-- format_on_save = {
+	--   -- These options will be passed to conform.format()
+	--   timeout_ms = 500,
+	--   lsp_fallback = true,
+	-- },
 }
 
 return options
--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,51 +3,68 @@ require("nvchad.configs.lspconfig").defaults()
 
 -- EXAMPLE
 local servers = {
-  html = {},
-  cssls = {},
-  bashls = {},
-  htmx = {
-    filetypes = {
-      "gohtml", "gohtmltmpl", "templ", "htmx"
-    }
-  },
-  tsserver = {},
-  gopls = {},
-  tailwindcss = {
-    filetypes = { "typescriptreact", "javascriptreact", "html", "htmx", "templ", "blade", "gohtml", "gohtmltmpl", "css", "less", "postcss", "sass", "scss" }
-  },
-  templ = {},
-  phpactor = {},
-  clangd = {},
-  prismals = {},
-  eslint = {
-    settings = {
-      workingDirectories = {
-        mode = "auto",
-      },
-    },
-  },
-  pyright = {
-    settings = {
-      python = {
-        analysis = {
-          autoSearchPaths = true,
-          typeCheckingMode = "basic",
-        },
-      },
-    },
-  },
-  lemminx = {}
+	html = {},
+	cssls = {},
+	bashls = {},
+	htmx = {
+		filetypes = {
+			"gohtml",
+			"gohtmltmpl",
+			"templ",
+			"htmx",
+		},
+	},
+	ts_ls = {},
+	gopls = {},
+	tailwindcss = {
+		filetypes = {
+			"typescriptreact",
+			"javascriptreact",
+			"html",
+			"htmx",
+			"templ",
+			"blade",
+			"gohtml",
+			"gohtmltmpl",
+			"css",
+			"less",
+			"postcss",
+			"sass",
+			"scss",
+		},
+	},
+	templ = {},
+	phpactor = {},
+	clangd = {},
+	prismals = {},
+	eslint = {
+		settings = {
+			workingDirectories = {
+				mode = "auto",
+			},
+		},
+	},
+	pyright = {
+		settings = {
+			python = {
+				analysis = {
+					autoSearchPaths = true,
+					typeCheckingMode = "basic",
+				},
+			},
+		},
+	},
+	lemminx = {},
 }
-local nvlsp = require "nvchad.configs.lspconfig"
+local nvlsp = require("nvchad.configs.lspconfig")
 
 -- lsps with default config
 for name, opts in pairs(servers) do
-  opts.on_init = nvlsp.on_init
-  opts.on_attach = nvlsp.on_attach
-  opts.capabilities = nvlsp.capabilities
+	opts.on_init = nvlsp.on_init
+	opts.on_attach = nvlsp.on_attach
+	opts.capabilities = nvlsp.capabilities
 
-  require("lspconfig")[name].setup(opts)
+	require("lspconfig")[name].setup(opts)
 end
 
 -- configuring single server, example: typescript
