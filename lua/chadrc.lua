fedc7f062436cff90189a74588505256863b6678
Hajir Asyafi <46818416+hajirasyafi@users.noreply.github.com>
2024-09-21 11:41:38 +0700

    Update chadrc.lua

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -7,42 +7,41 @@ local stbufnr = function()
   return vim.api.nvim_win_get_buf(vim.g.statusline_winid or 0)
 end
 M.base46 = {
-  theme = "kanagawa",
-  transparency = false,
+  theme = "bearded-arc",
 
-  -- hl_override = {
-  -- 	Comment = { italic = true },
-  -- 	["@comment"] = { italic = true },
-  -- },
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
 }
 -- M.lsp = { signature = false }
--- M.ui = {
---   statusline = {
---     theme = "default",
---     modules = {
---       relativepath = function()
---         local path = vim.api.nvim_buf_get_name(stbufnr())
---         if path == "" then
---           return ""
---         end
---         return "%#St_relativepath# " .. vim.fn.expand "%:.:h" .. " /"
---       end,
---     },
---     separator_style = "arrow",
---     order = {
---       "mode",
---       "relativepath",
---       "file",
---       "git",
---       "%=",
---       "lsp_msg",
---       "%=",
---       "diagnostics",
---       "lsp",
---       "cwd",
---       "cursor",
---     },
---   },
--- }
+M.ui = {
+  statusline = {
+    theme = "default",
+    modules = {
+      relativepath = function()
+        local path = vim.api.nvim_buf_get_name(stbufnr())
+        if path == "" then
+          return ""
+        end
+        return "%#St_relativepath# " .. vim.fn.expand "%:.:h" .. " /"
+      end,
+    },
+    order = {
+      "mode",
+      "relativepath",
+      "file",
+      "git",
+      "%=",
+      "lsp_msg",
+      "%=",
+      "diagnostics",
+      "lsp",
+      "cwd",
+      "cursor",
+    },
+  },
+}
+M.mason = { command = true, pkgs = {} }
 
 return M
