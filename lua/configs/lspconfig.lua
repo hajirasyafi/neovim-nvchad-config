c3616b8c964292d97e11359938b588e291d8fbc3
Hajir Asyafi <46818416+hajirasyafi@users.noreply.github.com>
2024-10-05 14:34:13 +0700

    Update lspconfig.lua

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,74 +1,77 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
+local lspconfig = require "lspconfig"
+
 -- EXAMPLE
 local servers = {
-	html = {},
-	cssls = {},
-	bashls = {},
-	htmx = {
-		filetypes = {
-			"gohtml",
-			"gohtmltmpl",
-			"templ",
-			"htmx",
-		},
-	},
-	ts_ls = {},
-	gopls = {},
-	tailwindcss = {
-		filetypes = {
-			"typescriptreact",
-			"javascriptreact",
-			"html",
-			"htmx",
-			"templ",
-			"blade",
-			"gohtml",
-			"gohtmltmpl",
-			"css",
-			"less",
-			"postcss",
-			"sass",
-			"scss",
-		},
-	},
-	templ = {},
-	phpactor = {},
-	clangd = {},
-	prismals = {},
-	eslint = {
-		settings = {
-			workingDirectories = {
-				mode = "auto",
-			},
-		},
-	},
-	pyright = {
-		settings = {
-			python = {
-				analysis = {
-					autoSearchPaths = true,
-					typeCheckingMode = "basic",
-				},
-			},
-		},
-	},
-	lemminx = {},
+  html = {},
+  cssls = {},
+  bashls = {},
+  htmx = {
+    filetypes = {
+      "gohtml",
+      "gohtmltmpl",
+      "templ",
+      "htmx",
+    },
+  },
+  ts_ls = {},
+  gopls = {},
+  tailwindcss = {
+    filetypes = {
+      "typescriptreact",
+      "javascriptreact",
+      "html",
+      "htmx",
+      "templ",
+      "blade",
+      "gohtml",
+      "gohtmltmpl",
+      "css",
+      "less",
+      "postcss",
+      "sass",
+      "scss",
+    },
+  },
+  templ = {},
+  phpactor = {},
+  clangd = {},
+  prismals = {},
+  eslint = {
+    settings = {
+      workingDirectories = {
+        mode = "auto",
+      },
+    },
+  },
+  pyright = {
+    settings = {
+      python = {
+        analysis = {
+          autoSearchPaths = true,
+          typeCheckingMode = "basic",
+        },
+      },
+    },
+  },
+  lemminx = {},
 }
-local nvlsp = require("nvchad.configs.lspconfig")
+
+local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
-for name, opts in pairs(servers) do
-	opts.on_init = nvlsp.on_init
-	opts.on_attach = nvlsp.on_attach
-	opts.capabilities = nvlsp.capabilities
-
-	require("lspconfig")[name].setup(opts)
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  }
 end
 
 -- configuring single server, example: typescript
--- lspconfig.tsserver.setup {
+-- lspconfig.ts_ls.setup {
 --   on_attach = nvlsp.on_attach,
 --   on_init = nvlsp.on_init,
 --   capabilities = nvlsp.capabilities,
